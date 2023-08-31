b9b8746e3446f425db3878f1434d9a6a10fae37f
LAGNER <egorlagner@gmail.com>
2023-08-31 14:22:06 +0300

    добавлены модели Worker и Department

--- stuff/models.py
+++ stuff/models.py
@@ -1,1 +1,22 @@
+from django.db import models
 
+
+class Department(models.Model):
+    """Модель подразделения/отделения/участка/департамента"""
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.location
+
+
+class Worker(models.Model):
+    """Модель трудоустроенного сотрудника"""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"({self.last_name.title()} {self.first_name.title()})"
