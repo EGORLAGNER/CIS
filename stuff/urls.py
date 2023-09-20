b6c9899f41cc64b76437e810343f288a1f74860b
LAGNER <egorlagner@gmail.com>
2023-09-20 10:42:40 +0300

    добавлен вывод списка всех сотрудников на страницу по адресу all_workers/

--- stuff/urls.py
+++ stuff/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from stuff.views import index
+from stuff.views import index, AllWorkers
 
 urlpatterns = [
     path('', index),
+    path('all_workers/', AllWorkers.as_view(), name='all_workers'),
 ]
--- stuff/views.py
+++ stuff/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.views.generic import View
+
+from stuff.models import *
 
 
 def index(request):
+    """Заглавная страница приложения. Отображает название приложения"""
     return HttpResponse('This my CIS - "Corporate Information System"')
+
+
+class AllWorkers(View):
+    """Показывает всех работников"""
+
+    def get(self, request):
+        all_workers = Worker.objects.all()
+        context = {'workers': all_workers}
+        return render(request, 'stuff/index.html', context)
