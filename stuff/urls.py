f1e50ad2f061c0b17b5c8661ab70a09ed6c8e10a
LAGNER <egorlagner@gmail.com>
2023-09-20 11:51:09 +0300

    изменено имя url шаблона главной страницы

--- stuff/urls.py
+++ stuff/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from stuff.views import Index, AllWorkers
 
 urlpatterns = [
-    path('', Index.as_view(), name='index_page_url'),
+    path('', Index.as_view(), name='main_page_url'),
     path('all_workers/', AllWorkers.as_view(), name='all_workers_url'),
 ]
