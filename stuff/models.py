5fc8c7d3330a7ceccff33c98850224cf090765ae
LAGNER <egorlagner@gmail.com>
2023-09-21 12:23:30 +0300

    добавлена модель Device отвечающая за устройства или оборудование закрепленное за работником

--- stuff/models.py
+++ stuff/models.py
@@ -11,15 +11,39 @@ class Department(models.Model):
 
 class Worker(models.Model):
     """Модель трудоустроенного сотрудника"""
+
     last_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     middle_name = models.CharField(max_length=255)
     # email = models.EmailField()
     # phone = models.CharField(max_length=20)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+
+    """Отношения между моделями"""
+    department = models.ForeignKey(
+        Department,  # главная модель с которой строятся отношения One to Many
+        on_delete=models.CASCADE,  # опция удаления
+        null=True,  # разрешает оставлять пустое поле в базе данных
+        blank=True,  # разрешает оставлять пустое поле при заполнении форм (не имеет отношения к базе данных)
+        related_name='department'  # присваивает связанное имя (если явно не указывать, то сгенерируется автоматически)
+    )
 
     def __str__(self):
         return f"({self.last_name.title()} {self.first_name.title()})"
 
     class Meta:
         ordering = ['last_name']
+
+
+class Device(models.Model):
+    """Модель устройства/оборудования необходимого для выполнения работы"""
+    name = models.CharField(max_length=255)
+    serial_number = models.CharField(max_length=255)
+    special_device = models.BooleanField(default=False)
+    inventory_number = models.PositiveBigIntegerField()
+    registered_to = models.ForeignKey(
+        Worker,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='worker'
+    )
